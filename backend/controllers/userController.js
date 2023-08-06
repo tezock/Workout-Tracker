71596bd7051739e927d132b3f7e00654a39ed99a
Robert Tezock <105029396+tezock@users.noreply.github.com>
2023-08-06 16:26:26 -0500

    Signing JWTs on proper user sign up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id: _id}, process.env.SECRET, { expiresIn: '3d'})
+
+}
 
 //login user
 const loginUser = async (req, res) => {
@@ -8,7 +14,20 @@ const loginUser = async (req, res) => {
 //sign up user
 
 const signupUser = async (req, res) => {
-    res.json({mssg: 'signup user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+
+        res.status(400).json({error: error.message})
+
+    }
 }
 
 module.exports = { signupUser, loginUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,41 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    // validation for email and password
+
+    if (!email || !password) {
+        throw Error('All fields mustbe filed')
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+
+    //get default values for determining if password is strong enough
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    //method that generates extra random characters to add onto a password to prevent password matching in the database
+    //higher the value, the longer it takes to hack
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
