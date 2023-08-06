119f06edd9f622db5ded33f0674dbc7314cef3ee
Robert Tezock <105029396+tezock@users.noreply.github.com>
2023-08-06 15:01:34 -0500

    Added user API call path

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel')
+
+//login user
+const loginUser = async (req, res) => {
+    res.json({mssg: 'login user'})
+}
+
+//sign up user
+
+const signupUser = async (req, res) => {
+    res.json({mssg: 'signup user'})
+}
+
+module.exports = { signupUser, loginUser }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
 
 //creates the express app
 const app = express()
@@ -18,6 +20,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use('/api/workouts/', workoutRoutes)
+app.use('/api/user/', userRoutes)
 
 //connect to database
 mongoose.connect(process.env.MONGO_URI)
