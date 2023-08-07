6b6195e0602789cfbbace7b67947d757482068b0
Robert Tezock <105029396+tezock@users.noreply.github.com>
2023-08-06 19:34:54 -0500

    login, logout, sign up hooks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header>
             <div className="container">
@@ -8,6 +16,9 @@ const Navbar = () => {
                     <h1>Workout Tracker</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log Out</button>
+                    </div>
                     <div>
                         <Link to='/login'>Log In</Link>
                         <Link to='/signup'>Sign Up</Link>
