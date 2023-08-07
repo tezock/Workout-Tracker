d9dfb1265c5b3161a5f59dcc9105484ce42deb9c
Robert Tezock <105029396+tezock@users.noreply.github.com>
2023-08-07 08:58:45 -0500

    Added finishing touches

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -32,7 +32,7 @@ const WorkoutDetails = ({ workout }) => {
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
-            <p><strong>Load (kg): </strong> {workout.load}</p>
+            <p><strong>Load (lbs): </strong> {workout.load}</p>
             <p><strong>Reps: </strong> {workout.reps}</p>
             <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
 
