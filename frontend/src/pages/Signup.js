5d4e6b7121c4793f6771e3c9e17f3a7057996c0f
Robert Tezock <105029396+tezock@users.noreply.github.com>
2023-08-09 16:28:23 -0500

    Build

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
                 value={password}
             />
 
-            <button disabled ={isLoading}>Sign Up</button>
+            <button disabled={isLoading}>Sign Up</button>
             {error && <div className='error'>{error}</div>}
         </form>
     )
